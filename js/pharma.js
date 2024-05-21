f307c678b3f1400d8fd1e5074b71db7b40d92534
kausik10 <kausikpaudel@gmail.com>
2024-05-21 21:59:26 +0545

    Basic Template

---
+++ js/pharma.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const menuBtn = document.getElementById('menu-btn');
+    const navbar = document.querySelector('.navbar');
+
+    menuBtn.addEventListener('click', () => {
+        navbar.classList.toggle('active');
+    });
+});
